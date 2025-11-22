5e511e987cc6ecac9686bdca53b6b5c245e96619
Victor Manuel Abad Pereda <victorm@xetid.cu>
2025-11-22 01:22:17 -0500

    Refactor email

--- src/constants/info.tsx
+++ src/constants/info.tsx
@@ -3,7 +3,7 @@ import { calculeAge } from '@/components/utils/about-me';
 export const INFO = {
   name: 'Victor Manuel Abad Pereda',
   age: calculeAge('1999-08-04'), // TODO: Update with your actual birth date
-  email: 'vabadpereda@gmail.com', // TODO: Update with your actual email
+  email: 'victicomanuelabadpereda@gmail.com', // TODO: Update with your actual email
 
   work: {
     year: 3
