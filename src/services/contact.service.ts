e914ef50026d4886a07616d725f1ec7adc6d0417
Victor Manuel Abad Pereda <victorm@xetid.cu>
2025-11-22 01:12:26 -0500

    Refactor email

--- src/services/contact.service.ts
+++ src/services/contact.service.ts
@@ -1,13 +1,19 @@
-import { EmailTemplate } from "@/definitions/contact";
+import { EmailTemplate } from '@/definitions/contact';
 
 export const SendEmail = async (payload: EmailTemplate) => {
-  const response = await fetch("/api/email", {
-    method: "POST",
+  const response = await fetch('/api/email', {
+    method: 'POST',
     headers: {
-      "Content-Type": "application/json",
+      'Content-Type': 'application/json'
     },
-    body: JSON.stringify(payload),
+    body: JSON.stringify(payload)
   });
 
-  return response.json();
+  const data = await response.json();
+
+  if (!response.ok) {
+    throw new Error(data.error || 'Error al enviar el email');
+  }
+
+  return data;
 };
