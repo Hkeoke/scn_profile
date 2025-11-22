1777005ea2f51b0088f8537b32729b8fb065779e
Victor Manuel Abad Pereda <victorm@xetid.cu>
2025-11-22 00:38:05 -0500

    Refactor PDF download logic

--- src/components/cv/cv-template.tsx
+++ src/components/cv/cv-template.tsx
@@ -1,42 +1,31 @@
-import { Document, Link, Page, Text, View } from "@react-pdf/renderer";
-import { TFunction } from "i18next";
-import { styles } from "./cv-styles";
-import { INFO } from "@/constants/info";
-import { parseI18nText } from "./cv-text-parser";
-import { ITimeLine } from "@/definitions/resumen";
-import { PropsWithChildren } from "react";
-import { codingSkills, SkillProps, tools } from "@/constants/skill";
+import { Document, Link, Page, Text, View } from '@react-pdf/renderer';
+import { TFunction } from 'i18next';
+import { styles } from './cv-styles';
+import { INFO } from '@/constants/info';
+import { parseI18nText } from './cv-text-parser';
+import { ITimeLine } from '@/definitions/resumen';
+import { PropsWithChildren } from 'react';
+import { codingSkills, SkillProps, tools } from '@/constants/skill';
 
 type Props = {
-  t: TFunction<["translation", ...string[]], undefined>;
+  t: TFunction<['translation', ...string[]], undefined>;
 };
 export const CvTemplate = ({ t }: Props) => {
-  const description = t("about-me:aboutMe.description", {
+  const description = t('about-me:aboutMe.description', {
     name: INFO.name,
     years: INFO.work.year,
-    interpolation: { escapeValue: false },
+    interpolation: { escapeValue: false }
   });
-  const educations =
-    (t("education:educations", { returnObjects: true }) as ITimeLine[]) ?? [];
-  const experiences =
-    (t("experience:experiences", { returnObjects: true }) as ITimeLine[]) ?? [];
+  const educations = (t('education:educations', { returnObjects: true }) as ITimeLine[]) ?? [];
+  const experiences = (t('experience:experiences', { returnObjects: true }) as ITimeLine[]) ?? [];
 
   return (
     <Document>
       <Page size="A4" style={[styles.page, { paddingBottom: 60 }]}>
         <View style={styles.section}>
-          <Text style={[styles.h1, { textAlign: "center" }]}>{INFO?.name}</Text>
-          <View
-            style={[
-              styles.flexRow,
-              styles.mb10,
-              styles.border,
-              { justifyContent: "center" },
-            ]}
-          >
-            <Text>{t("common:address")}</Text>
-            <Point />
-            <Link>{"https://www.linkedin.com/in/socarrandin/"}</Link> {/* TODO: Update with your LinkedIn profile */}
+          <Text style={[styles.h1, { textAlign: 'center' }]}>{INFO?.name}</Text>
+          <View style={[styles.flexRow, styles.mb10, styles.border, { justifyContent: 'center' }]}>
+            <Text>{t('common:address')}</Text>
             <Point />
             <Link href={`mailto:${INFO.email}`}>{INFO.email}</Link>
           </View>
@@ -46,17 +35,15 @@ export const CvTemplate = ({ t }: Props) => {
 
           {/* experiences */}
           <View style={[styles.mt10]}>
-            <Header>{t("experience:title")}</Header>
+            <Header>{t('experience:title')}</Header>
             <View style={[styles.flexCol, { gap: 16 }]}>
-              {experiences?.map((item, index) => (
-                <Item key={index} {...item} />
-              ))}
+              {experiences?.map((item, index) => <Item key={index} {...item} />)}
             </View>
           </View>
 
           {/* education */}
           <View style={[styles.mt10]}>
-            <Header>{t("education:title")}</Header>
+            <Header>{t('education:title')}</Header>
             <View style={[styles.flexCol, { gap: 16 }]}>
               {educations?.map((item, index) => <Item key={index} {...item} />)}
             </View>
@@ -64,19 +51,17 @@ export const CvTemplate = ({ t }: Props) => {
 
           {/* skill code */}
           <View style={[styles.mt10]}>
-            <Header>{t("resumen:skills.code")}</Header>
+            <Header>{t('resumen:skills.code')}</Header>
 
-            <View style={[styles.flexRow, { flexWrap: "wrap", gap: 4 }]}>
-              {codingSkills?.map((item, index) => (
-                <SkillItem key={index} {...item} />
-              ))}
+            <View style={[styles.flexRow, { flexWrap: 'wrap', gap: 4 }]}>
+              {codingSkills?.map((item, index) => <SkillItem key={index} {...item} />)}
             </View>
           </View>
           {/* skill tools */}
           <View style={[styles.mt10]}>
-            <Header>{t("resumen:skills.tools")}</Header>
+            <Header>{t('resumen:skills.tools')}</Header>
 
-            <View style={[styles.flexRow, { flexWrap: "wrap", gap: 4 }]}>
+            <View style={[styles.flexRow, { flexWrap: 'wrap', gap: 4 }]}>
               {tools?.map((item, index) => <SkillItem key={index} {...item} />)}
             </View>
           </View>
@@ -92,15 +77,12 @@ const Item = (item: ITimeLine) => {
   const form = getFormat(item?.dateRange?.from);
   const to = getFormat(item?.dateRange?.to);
 
-  const _description =
-    typeof item?.description === "string"
-      ? [item?.description]
-      : item?.description;
+  const _description = typeof item?.description === 'string' ? [item?.description] : item?.description;
 
   return (
     <View>
-      <View style={[styles.flexRow, { justifyContent: "space-between" }]}>
-        <View style={{ width: "80%" }}>
+      <View style={[styles.flexRow, { justifyContent: 'space-between' }]}>
+        <View style={{ width: '80%' }}>
           <Text style={[styles.bold]}>{item?.companyName}</Text>
           <Text>{item?.jobTitle}</Text>
         </View>
@@ -116,9 +98,7 @@ const Item = (item: ITimeLine) => {
         {_description?.map((desc, index) => (
           <View key={index} style={[styles.customBullet]}>
             <View style={styles.bullet} />
-            <Text style={[styles.mt5, { fontWeight: "light" }]}>
-              {parseI18nText(desc)}
-            </Text>
+            <Text style={[styles.mt5, { fontWeight: 'light' }]}>{parseI18nText(desc)}</Text>
           </View>
         ))}
       </View>
@@ -128,27 +108,17 @@ const Item = (item: ITimeLine) => {
 
 const Header = ({ children }: PropsWithChildren) => {
   return (
-    <Text
-      style={[
-        styles.border,
-        styles.mt10,
-        styles.mb10,
-        styles.bold,
-        { textTransform: "uppercase" },
-      ]}
-    >
+    <Text style={[styles.border, styles.mt10, styles.mb10, styles.bold, { textTransform: 'uppercase' }]}>
       {children}
     </Text>
   );
 };
 const SkillItem = (item: SkillProps) => {
-  return (
-    <Text style={[styles.skill, { fontWeight: "light" }]}>{item?.name}</Text>
-  );
+  return <Text style={[styles.skill, { fontWeight: 'light' }]}>{item?.name}</Text>;
 };
 
 const getFormat = (date: string) => {
-  if (date === null) return new Date().toISOString().split("T")[0];
+  if (date === null) return new Date().toISOString().split('T')[0];
 
-  return new Date(date).toISOString().split("T")[0];
+  return new Date(date).toISOString().split('T')[0];
 };
--- src/constants/info.tsx
+++ src/constants/info.tsx
@@ -3,7 +3,7 @@ import { calculeAge } from '@/components/utils/about-me';
 export const INFO = {
   name: 'Victor Manuel Abad Pereda',
   age: calculeAge('1999-08-04'), // TODO: Update with your actual birth date
-  email: 'arrebatao007@gmail.com', // TODO: Update with your actual email
+  email: 'vabadpereda@gmail.com', // TODO: Update with your actual email
 
   work: {
     year: 3
