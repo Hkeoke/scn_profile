e5a3791370eb91d5ba2b9289c9faf9b7c9e3ad7b
Victor <victorm@xetid.cu>
2026-01-08 19:35:32 -0500

    Add generateStaticParams to catch-all route

--- src/app/[locale]/[...rest]/page.tsx
+++ src/app/[locale]/[...rest]/page.tsx
@@ -1,4 +1,12 @@
-import {notFound} from 'next/navigation';
+import { notFound } from 'next/navigation';
+import i18nConfig from '../../../../next-i18next.config';
+
+export function generateStaticParams() {
+  return i18nConfig.locales.map(locale => ({
+    locale,
+    rest: ['404']
+  }));
+}
 
 export default function CatchAllPage() {
   notFound();
